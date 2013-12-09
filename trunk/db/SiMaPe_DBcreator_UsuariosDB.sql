727706cfff271cf5957f8236725d87718f79a6c1
ivan.barrera.oro@gmail.com <ivan.barrera.oro@gmail.com@d83696aa-246a-b927-6463-8bf85c3dbaf6>
2013-12-09 17:47:49 +0000

    SiMaPe DB MySQL script v1.1 - User Creation.  Se recomienda no usar solo un admin, sino crear la cantidad que corresponda.  Se recomienda NO USAR el usuario root.

---
+++ trunk/db/SiMaPe_DBcreator_UsuariosDB.sql
@@ -0,0 +1,15 @@
+-- -----------------------------------------------------
+-- Usuarios
+-- -----------------------------------------------------
+
+-- Admin
+CREATE USER 'admin'@'localhost' IDENTIFIED BY PASSWORD '<encrypted_pass: SELECT PASSWORD('mipassword')>';
+GRANT ALL PRIVILEGES ON SiMaPe.* TO 'admin'@'localhost';
+
+-- Application Read Only
+CREATE USER 'appro'@'localhost' IDENTIFIED BY PASSWORD '<encrypted_pass: SELECT PASSWORD('mipassword')>';
+GRANT SELECT ON SiMaPe.* TO 'appro'@'localhost';
+
+-- Application ReadWrite
+CREATE USER 'apprw'@'localhost' IDENTIFIED BY PASSWORD '<encrypted_pass: SELECT PASSWORD('mipassword')>';
+GRANT SELECT, UPDATE, INSERT, DELETE ON SiMaPe.* TO 'apprw'@'localhost';
