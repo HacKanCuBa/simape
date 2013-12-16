114197b0b7827602516b17d34efc6c0b3c836e5b
ivan.barrera.oro@gmail.com <ivan.barrera.oro@gmail.com@d83696aa-246a-b927-6463-8bf85c3dbaf6>
2013-12-16 22:38:11 +0000

    class_db v0.9: primera versión totalmente funcional

--- trunk/website/inc/class_db.php
+++ trunk/website/inc/class_db.php
@@ -19,53 +19,106 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * 
- * @license GPL-3.0+ <http://spdx.org/licenses/GPL-3.0+>
- * 
  *****************************************************************************/
 
 /**
- * Esta clase maneja todo lo referido a la DB:
- * - Conexion RO
- * - Conexion RW
- * - Querys
- * - etc
+ * Maneja la conexión con la DB y la ejecución de consultas.
+ * 
+ * Ejemplo de uso:
+ * <pre><code>
+ * $db = new DB(); // modo RO
+ * $db->setQuery('SELECT * FROM mitabla WHERE micolumna = ?');
+ * $db->setBindParam('s');
+ * $db->setQueryParams('valor');
+ * $db->queryExecute();
+ * if ($db->getAffectedRows()) {
+ *  misDatos = $db->getQueryData();
+ *  echo "Se obtuvieron " . $db->getAffectedRows() . " filas.";
+ * }
+ * $db->cambiarModo(TRUE);  //modo RW
+ * $db->setQuery('INSERT INTO mitabla (micolumna) VALUES (?)');
+ * $db->setBindParam('s');
+ * $db->setQueryParams('NuevoValor');
+ * if ($db->queryExecute()) {
+ *  echo "Se obtuvieron " . $db->getAffectedRows() . " filas.";
+ * }
+ * </code></pre>
  *
- * @version 0.7 untested
+ * @author Iván A. Barrera Oro <ivan.barrera.oro@gmail.com>
+ * @copyright (c) 2013, Iván A. Barrera Oro
+ * @license http://spdx.org/licenses/GPL-3.0+ GNU GPL v3.0
+ * @version 0.9
  */
-
-class DB {
-    protected $db;
+class DB extends mysqli
+{  
     protected $queryStmt, $bindParam, $queryParams;
-    
+    protected $queryData;
+    protected $affectedRows;
+
+
     // Metodos
     // __ SPECIALS
+    
+    /**
+     * Conecta con la DB según el modo indicado e inicializa la conexión.
+     * 
+     * @param boolean $ModoRW Establece el modo RW si es TRUE, 
+     * RO si es FALSE.
+     */
     function __construct($ModoRW = FALSE) 
-    {
-        /**
-         * Genera un objeto mysqli y conecta en el modo indicado.
-         * 
-         * @param boolean $ModoRW Si es TRUE, conecta en modo RW; si es FALSE,
-         * en modo RO (por defecto).
-         */
-       
-        if ($ModoRW) {
-            $db = new mysqli(DB_HOST, DB_USUARIO_RW, DB_PASS_RW, DB_NOMBRE);
-        } else {
-            $db = new mysqli(DB_HOST, DB_USUARIO_RO, DB_PASS_RO, DB_NOMBRE);
+    {   
+        if (!$this->conexionEstablecer($ModoRW) 
+            || !$this->conexionInicializar()) {
+            trigger_error('Error de conexion', E_USER_ERROR);
         }
         
-        $this->db = $db;
-        $this->setCharset();
     }
     
+//    function __destruct() 
+//    {
+//        if (!empty($this->queryStmt)) {
+//            $this->queryStmt->close();
+//        }
+//        parent::close();
+//    }
+
     // __ PRIV
     
     // __ PROT
+    /**
+     * Conecta en el modo indicado.
+     * 
+     * @param boolean $ModoRW Si es TRUE, conecta en modo RW; si es FALSE,
+     * en modo RO (por defecto).
+     * @return boolean TRUE si se estableció la conexión exitosamente, 
+     * FALSE si no.
+     */
+    protected function conexionEstablecer($ModoRW = FALSE)
+    {  
+        if ($ModoRW) {
+            parent::__construct(DB_HOST, DB_USUARIO_RW, DB_PASS_RW, DB_NOMBRE);
+        } else {
+            parent::__construct(DB_HOST, DB_USUARIO_RO, DB_PASS_RO, DB_NOMBRE);
+        }
+        
+        if ($this->connect_error) {
+            return FALSE;
+        } else {
+            return TRUE;
+        }
+    }
+    
+    /**
+     * Configura el charset de la DB.
+     * 
+     * @return boolean TRUE si se configuró correctamente el charset, 
+     * FALSE si no.
+     */
     protected function setCharset() 
     {
-        if ($this->db->character_set_name() != constant('DB_CHARSET')) {
-            if ($this->db->set_charset(constant('DB_CHARSET'))) {
-                return $this->db->real_query('SET NAMES ' . constant('DB_CHARSET'));
+        if ($this->character_set_name() != constant('DB_CHARSET')) {
+            if ($this->set_charset(constant('DB_CHARSET'))) {
+                return $this->real_query('SET NAMES ' . constant('DB_CHARSET'));
             } else {
                 return FALSE;
             }
@@ -74,153 +127,44 @@ class DB {
         }
     }
     
-    // __ PUB
-    // Set
-    public function setQuery(string $queryPrepared) 
-    {
-        /**
-         * Prepara el objeto para ejecutar la query indicada.  Devuelve TRUE
-         * si se preparo exitosamente, FALSE en caso contrario.
-         * 
-         * @param string $queryPrepared Query a ser ejecutada.  Debe ser  
-         * Prepared Statement, a menos que no contenga parámetros.  NOTA: los
-         * statements (<?>) ¡no van encomillados!.
-         * @return boolean TRUE si tuvo éxito, FALSE si no.
-         */
-        
-        if (!empty($queryPrepared) && is_string($queryPrepared)) {
-            $stmt = $this->db->prepare($queryPrepared);
-            if($stmt) {
-                $this->queryStmt = $stmt;
-                return TRUE;
-            }
-        }
-        
-        return FALSE;
+    /**
+     * Ejecuta las tareas que deben realizarse luego de establecer una 
+     * conexión.
+     */
+    protected function conexionInicializar()
+    {        
+        return $this->setCharset();
     }
     
-    public function setBindParam(string $bindParam) 
-    {
-        /**
-         * Almacena los parametros de binding para la query.  Devuelve TRUE
-         * si tuvo éxito, FALSE si no.
-         * 
-         * @param string $bindParam Parametros para indicar el tipo de
-         * binding: i: integer; s: string; d: double; b: blob
-         * @return boolean Devuelve TRUE si tuvo éxito, FALSE si no.
-         */
-        
-        if (!empty($bindParam) && is_string($bindParam)) {
-            $this->bindParam = $bindParam;
-            return TRUE;
-        }
-        
-        return FALSE;
-    }
-    
-    public function setQueryParams(array $queryParams)
-    {
-        /**
-         * Almacena los parámetros de la query que reemplazan a los statements 
-         * (<?>).  ¡Debe ser un array!.  Devuelve TRUE si tuvo éxito, 
-         * FALSE si no.
-         * 
-         * @param array $queryParams Valores que serán insertados 
-         * en la query.
-         * @return boolean Devuelve TRUE si tuvo éxito, FALSE si no.
-         */
-        
-        if (!empty($queryParams) && is_array($queryParams)) {
-            $this->queryParams = $queryParams;
-            return TRUE;
-        }
-        
-        return FALSE;
-    }
-    // --
-    // Get
-    
-    // --
-    // Query
-    public function queryExecute() 
-    {
-        /**
-         * Ejecuta una query (SELECT, INSERT, UPDATE, DELETE) con o sin 
-         * parámetros usando transacciones.
-         * Devuelve TRUE si tuvo éxito, y ejecuta commit, o FALSE si no, y 
-         * hace rollback.
-         * Para obtener los datos de un SELECT, llamar a queryGetData().
-         * 
-         * @see DB::queryGetData()
-         * 
-         * @return boolean TRUE si tuvo éxito, FALSE si no.
-         */
-
-        $result = FALSE;
-
-        if(isset($this->db) && isset($this->queryStmt)) {
-            $this->db->autocommit(FALSE);
-
-            if(!empty($this->bindParam) && !empty($this->queryParams)) {
-                $bind_names[] = $this->bindParam;
-                for ($i = 0; $i < count($this->queryParams); $i++) {
-                    $bind_name = 'bind' . $i;
-                    $$bind_name = $this->queryParams[$i];
-                    $bind_names[] = &$$bind_name;
-                }
-                $result = call_user_func_array(array($this->queryStmt, 'bind_param'), $bind_names);
-            } else {
-                // Continuar, no hay que bindear la query
-                $result = TRUE;
-            }
-            
-            if($result) {
-                $result = $this->queryStmt->execute();
-                if ($result) {
-                    $this->db->commit();
-                } else {
-                    $this->db->rollback();
-                }
-            }
-
-            $this->db->autocommit(TRUE);
-        }
-
-        return $result;
-    }
-    
-    public function queryGetData() 
-    {
-        /**
-         * Devuelve los datos obtenidos de la query (cuando la misma se trató 
-         * de un SELECT) como array.  Si no se obtuvieron datos, devuelve TRUE.
-         * Si la consulta devolvió error, devuelve FALSE.
-         * El array devuelto es:
-         *  - Asociativo cuando la consulta devuelve una fila con multiples 
-         * columnas;
-         *  - Numerado cuando la consulta devuelve una o varias filas c/u con 
-         * sólo una columna; 
-         *  - Mixto cuando la consulta devuelve multiples filas con multiples 
-         * columnas (las filas serán índice numerado, y dentro un array 
-         * asociativo con las columnas como indices).
-         * 
-         * IMPORTANTE: Requiere native driver (php5-mysqlnd)
-         * ATENCIÓN: el resultado debería evaluarse con is_array() para 
-         * determinar si se obtuvieron datos en la consulta.
-         *
-         * @return mixed Si la consulta produjo resultados los devuelve como 
-         * array; si no hubieron resultados pero la consulta fue exitosa, 
-         * devuele TRUE; en caso de error, FALSE.
-         *  
-         */
-        
+    /**
+     * Guarda los datos obtenidos de la query (cuando la misma se trató 
+     * de un SELECT) de la siguiente manera:
+     * Si la consulta devolvió datos, como array.
+     * Si no se obtuvieron datos, como TRUE.
+     * Si la consulta devolvió error, como FALSE.
+     * El array devuelto es:
+     *  - Asociativo cuando la consulta devuelve una fila con multiples 
+     * columnas;
+     *  - Numerado cuando la consulta devuelve una o varias filas c/u con 
+     * sólo una columna; 
+     *  - Mixto cuando la consulta devuelve multiples filas con multiples 
+     * columnas (las filas serán índice numerado, y dentro un array 
+     * asociativo con las columnas como indices).
+     * 
+     * IMPORTANTE: Requiere native driver (php5-mysqlnd)
+     * ATENCIÓN: el resultado debería evaluarse con is_array() para 
+     * determinar si se obtuvieron datos en la consulta.
+     *  
+     */
+    protected function querySaveData() {        
         $result = FALSE;
         
         if (isset($this->queryStmt) && ($this->queryStmt->errno == 0)) {  
             $result = TRUE;
             
             $data = $this->queryStmt->get_result();
-            $this->queryStmt->close();
+            // no conviene cerrar acá este parámetro...
+            //$this->queryStmt->close();
             if ($data) {
                 // puede que haya datos
                 while ($row = $data->fetch_assoc()) {
@@ -243,21 +187,221 @@ class DB {
             }
         }
         
-        return $result;
+        if ($result) {
+            $this->queryData = $result;
+        } else {
+            unset($this->queryData);
+        }
+    }
+    // __ PUB
+//    public function close()
+//    {
+//        $this->__destruct();
+//    }
+
+    /**
+     * Cambia el modo de conexión con la DB.
+     * 
+     * @param boolean $ModoRW Establece el modo RW si es TRUE, 
+     * RO si es FALSE.
+     * @return boolean TRUE si se cambió el modo exitosamente, FALSE si no.
+     */
+    public function cambiarModo($ModoRW = FALSE) 
+    {
+        if ($ModoRW) {
+            $this->change_user(DB_USUARIO_RW, DB_PASS_RW, DB_NOMBRE);
+        } else {
+            $this->change_user(DB_USUARIO_RO, DB_PASS_RO, DB_NOMBRE);
+        }
+        
+        return $this->conexionInicializar();
+    }
+    
+    // Set
+    /**
+     * Prepara el objeto para ejecutar la query indicada.  Devuelve TRUE
+     * si se preparó exitosamente, FALSE en caso contrario.
+     * 
+     * @param string $queryPrepared Query a ser ejecutada.  Debe ser  
+     * Prepared Statement, a menos que no contenga parámetros.  NOTA: los
+     * statements (<i>?</i>) ¡no van encomillados!.
+     * @return boolean TRUE si tuvo éxito, FALSE si no.
+     */
+    public function setQuery($queryPrepared) 
+    {
+        $stmt = $this->prepare($queryPrepared);
+        if($stmt) {
+            $this->queryStmt = $stmt;
+            return TRUE;
+        }
+        
+        return FALSE;
+    }
+    
+    /**
+     * Almacena los parametros de binding para la query.  Devuelve TRUE
+     * si tuvo éxito, FALSE si no.
+     * 
+     * @param string $bindParam Parametros para indicar el tipo de
+     * binding: i: integer; s: string; d: double; b: blob
+     * @return boolean Devuelve TRUE si tuvo éxito, FALSE si no.
+     */
+    public function setBindParam($bindParam) 
+    {        
+        if (!empty($bindParam) 
+            && is_string($bindParam)
+            && !preg_match('/[^idsb]/', $bindParam)
+        ) {
+            $this->bindParam = $bindParam;
+            return TRUE;
+        }
+        
+        return FALSE;
+    }
+    
+    /**
+     * Almacena los parámetros de la query que reemplazan a los statements 
+     * (<i>?</i>).  Devuelve TRUE si tuvo éxito, 
+     * FALSE si no.
+     * 
+     * @param mixed $queryParams Valores que serán insertados 
+     * en la query.
+     * @return boolean Devuelve TRUE si tuvo éxito, FALSE si no.
+     */
+    public function setQueryParams($queryParams)
+    {    
+        if (!empty($queryParams)) {
+            if (is_array($queryParams)) {
+                $queryP = $queryParams;
+            } else {
+                $queryP = array($queryParams);
+            }
+            $this->queryParams = $queryP;
+            return TRUE;
+        }
+        
+        return FALSE;
     }
     // --
+    // Get
+    /**
+     * Devuelve la cantidad de filas afectadas por la última consulta, si es 
+     * que se trató de INSERT, UPDATE, REPLACE o DELETE.
+     * Devolverá la cantidad de filas del resultado si se trató de un SELECT.
+     * 
+     * @return int La cantidad de filas afectadas por la última consulta, si es 
+     * que se trató de INSERT, UPDATE, REPLACE o DELETE.  Devolverá la cantidad 
+     * de filas del resultado si se trató de un SELECT.
+     */
+    public function getAffectedRows()
+    {
+        if (isset($this->affectedRows)) {
+            return (int) $this->affectedRows;
+        }
+    }
+    
+    /**
+     * Devuelve los datos obtenidos en la última query, asumiendo que
+     * haya sido un SELECT.
+     *          
+     * @return mixed Si la consulta produjo resultados los devuelve como 
+     * array; si no hubieron resultados pero la consulta fue exitosa, 
+     * devuelve TRUE; en caso de error, FALSE.
+     */
+    public function getQueryData() 
+    {        
+        if (isset($this->queryData)) {
+            return $this->queryData;
+        }
+        
+        return FALSE;
+    }
+    // --
+    // Query
+    /**
+     * Ejecuta una query (SELECT, INSERT, UPDATE, DELETE) con o sin 
+     * parámetros usando transacciones.
+     * Devuelve TRUE si tuvo éxito, y ejecuta commit, o FALSE si no, y 
+     * hace rollback.  Si se produjo un error durante la ejecución de la query,
+     * devuelve el nro. de error en lugar de FALSE.
+     * Para obtener los datos de un SELECT, llamar a queryGetData().
+     * Para obtener la cantidad de filas afectadas, llamar a getAffectedRows().
+     * 
+     * @see queryGetData()
+     * @see getAffectedRows().
+     * @return mixed TRUE si tuvo éxito, FALSE si se produjo un error antes
+     * de la ejecución de la query.  Si el error se produjo durante, devuelve
+     * el nro. de error.
+     */
+    public function queryExecute() 
+    {
+        $result = FALSE;
+
+        if (!empty($this->queryStmt)) {
+            $this->autocommit(FALSE);
+
+            if(!empty($this->bindParam) && !empty($this->queryParams)) {
+                $bind_names[] = $this->bindParam;
+                for ($i = 0; $i < count($this->queryParams); $i++) {
+                    $bind_name = 'bind' . $i;
+                    $$bind_name = $this->queryParams[$i];
+                    $bind_names[] = &$$bind_name;
+                }
+                $result = call_user_func_array(array($this->queryStmt, 
+                                                     'bind_param'), 
+                                               $bind_names);
+            } else {
+                // Continuar, no hay que bindear la query
+                $result = TRUE;
+            }
+            
+            if($result) {
+                $result = $this->queryStmt->execute();
+                if ($result) {
+                    $this->commit();
+                    $this->querySaveData();
+                } else {
+                    $this->rollback();
+                    $result = $this->queryStmt->errno;
+                    
+                }
+            }
+            $this->affectedRows = $this->queryStmt->affected_rows;
+            $this->queryStmt->close();
+            $this->autocommit(TRUE);
+        }
+
+        return $result;
+    }
+    
+    /**
+     * Devuelve los datos obtenidos en la última query, asumiendo que
+     * haya sido un SELECT.
+     * Es un alias de getQueryData().
+     *          
+     * @see getQueryData()
+     * @return mixed Si la consulta produjo resultados los devuelve como 
+     * array; si no hubieron resultados pero la consulta fue exitosa, 
+     * devuelve TRUE; en caso de error, FALSE.
+     */
+    public function queryGetData()
+    {
+        return $this->getQueryData();
+    }
+
+    // --
     // Otras
+    /**
+     * Sanitiza un valor para ser usado en una consulta.  Puede tratarse de
+     * un array o un string.
+     * NOTA: No es necesario si se emplea una prepared query.
+     *
+     * @param mixed $valor Valor que será sanitizado.
+     * @return mixed Valor sanitizado para ser usado en una consulta.  Será
+     * del mismo tipo que $valor, o bien NULL en caso de error
+     */
     public function sanitizar($valor) 
     {
-        /**
-         * Sanitiza un valor para ser usado en una consulta.  Puede tratarse de
-         * un array o un string.
-         * NOTA: No es necesario si se emplea una prepared query.
-         *
-         * @param mixed $valor Valor que será sanitizado.
-         * @return mixed Valor sanitizado para ser usado en una consulta.  Será
-         * del mismo tipo que $valor, o bien NULL en caso de error
-         */
         if ($this->setCharset() && !empty($valor)) {
             if (is_array($valor)) {
                 foreach ($valor as $key => $value) {
@@ -265,7 +409,7 @@ class DB {
                 }
             } else {
                 $result = addcslashes(
-                            $this->db->real_escape_string(stripslashes($valor))
+                            $this->real_escape_string(stripslashes($valor))
                             , '%_');
             }
             return $result;
