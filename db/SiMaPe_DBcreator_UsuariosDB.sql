3311f53f99640eb40a9c38bc5f6f8af59de02e07
ivan.barrera.oro@gmail.com <ivan.barrera.oro@gmail.com@d83696aa-246a-b927-6463-8bf85c3dbaf6>
2013-12-23 01:25:46 +0000

    >> DB + GRANT OPTION to admin + ALTER, CREATE to apprw

--- db/SiMaPe_DBcreator_UsuariosDB.sql
+++ db/SiMaPe_DBcreator_UsuariosDB.sql
@@ -4,7 +4,7 @@
 
 -- Admin
 CREATE USER 'admin'@'localhost' IDENTIFIED BY PASSWORD '<encrypted_pass: SELECT PASSWORD('mipassword')>';
-GRANT ALL PRIVILEGES ON SiMaPe.* TO 'admin'@'localhost';
+GRANT ALL PRIVILEGES ON SiMaPe.* TO 'admin'@'localhost' WITH GRANT OPTION;
 
 -- Application Read Only
 CREATE USER 'appro'@'localhost' IDENTIFIED BY PASSWORD '<encrypted_pass: SELECT PASSWORD('mipassword')>';
@@ -12,4 +12,4 @@ GRANT SELECT ON SiMaPe.* TO 'appro'@'localhost';
 
 -- Application ReadWrite
 CREATE USER 'apprw'@'localhost' IDENTIFIED BY PASSWORD '<encrypted_pass: SELECT PASSWORD('mipassword')>';
-GRANT SELECT, UPDATE, INSERT, DELETE ON SiMaPe.* TO 'apprw'@'localhost';
+GRANT SELECT, UPDATE, INSERT, DELETE, ALTER, CREATE ON SiMaPe.* TO 'apprw'@'localhost';
