f0a4ff68486b79d8d7af80c436955f04026111b4
ivan.barrera.oro@gmail.com <ivan.barrera.oro@gmail.com@d83696aa-246a-b927-6463-8bf85c3dbaf6>
2014-09-30 16:45:24 +0000

    GENERAL: rediseño de la estructura de directorios.  no compatible con version anterior.  requiere reinstalacion completa. saper.php: correccion de errores. saperficha_class.php: eliminacion de columna 'extras' del saper, porque causa confusion; columna 'faltan' tambien porque la calculo.  mejora en el calculo de hs extra pdf.css: correccion de errores. Nota: mpdf no interpreta del todo correcto los archivos css.

---
+++ website/usr/adm/autoload.php
@@ -0,0 +1,53 @@
+<?php
+
+/*****************************************************************************
+ *  Este archivo forma parte de SiMaPe
+ *  Sistema Integrado de Manejo de Personal
+ *  Copyright (C) <2013, 2014>  <Ivan Ariel Barrera Oro>
+ *  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *
+ *  SiMaPe is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  SiMaPe is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ *****************************************************************************/
+
+/**
+ * autoload.php
+ * Busca y carga load.php, que estará en la raíz.
+ * Debe estar en todos los subdirectorios con páginas ejecutables (esto es, 
+ * donde haya código a ejecutar, ¡NO en librerias o inclusiones!).  La idea
+ * es que sea más fácil rediseñar el sitio y reacomodar los archivos.
+ * 
+ * @author Iván A. Barrera Oro <ivan.barrera.oro@gmail.com>
+ * @copyright (c) 2014, Iván A. Barrera Oro
+ * @license http://spdx.org/licenses/GPL-3.0+ GNU GPL v3.0
+ * @version 0.2
+ */
+
+// Para forzar la ruta de config.php, borrar '//' de las siguientes 2 lineas:
+//require_once $location . '../load.php;
+///* Buscar load.php
+$location = dirname(__FILE__);
+do {
+    if (file_exists($location . '/load.php')) {
+        require_once $location . '/load.php';
+        $location = '/';
+    } else {
+        $location = dirname($location);
+    }
+} while ($location != '/');
+// -- */
+
+if (!defined('SMP_CONFIG')) { 
+    die("No se puede encontrar el archivo load.php"); 
+}
