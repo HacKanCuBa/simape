16b06e578b7bc943eb9c6a5e70f668b0eb638a1c
ivan.barrera.oro@gmail.com <ivan.barrera.oro@gmail.com@d83696aa-246a-b927-6463-8bf85c3dbaf6>
2014-11-07 23:21:22 +0000

    db v2.11: integracion freichat

--- trunk/db/SiMaPe_DBcreator_UsuariosDB.sql
+++ trunk/db/SiMaPe_DBcreator_UsuariosDB.sql
@@ -13,3 +13,15 @@ GRANT SELECT ON SiMaPe.* TO 'appro'@'localhost';
 -- Application ReadWrite
 CREATE USER 'apprw'@'localhost' IDENTIFIED BY PASSWORD '<encrypted_pass: SELECT PASSWORD('mipassword')>';
 GRANT SELECT, UPDATE, INSERT, DELETE, CREATE TEMPORARY TABLES ON SiMaPe.* TO 'apprw'@'localhost';
+
+-- Chat User
+CREATE USER 'chat'@'localhost' IDENTIFIED BY PASSWORD '<encrypted_pass: SELECT PASSWORD('mipassword')>';
+GRANT SELECT, UPDATE, INSERT, DELETE ON `SiMaPe`.`frei_banned_users` TO 'chat'@'localhost';
+GRANT SELECT, UPDATE, INSERT, DELETE ON `SiMaPe`.`frei_chat` TO 'chat'@'localhost';
+GRANT SELECT, UPDATE, INSERT, DELETE ON `SiMaPe`.`frei_config` TO 'chat'@'localhost';
+GRANT SELECT, UPDATE, INSERT, DELETE ON `SiMaPe`.`frei_rooms` TO 'chat'@'localhost';
+GRANT SELECT, UPDATE, INSERT, DELETE ON `SiMaPe`.`frei_session` TO 'chat'@'localhost';
+GRANT SELECT, UPDATE, INSERT, DELETE ON `SiMaPe`.`frei_smileys` TO 'chat'@'localhost';
+GRANT SELECT, UPDATE, INSERT, DELETE ON `SiMaPe`.`frei_video_session` TO 'chat'@'localhost';
+GRANT SELECT, UPDATE, INSERT, DELETE ON `SiMaPe`.`frei_webrtc` TO 'chat'@'localhost';
+GRANT SELECT ON `SiMaPe`.`frei_users` TO 'chat'@'localhost';
