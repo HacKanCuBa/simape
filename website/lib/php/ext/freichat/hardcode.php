9a82aa20bf3f2807220f9495fbd4ca40ed785af8
ivan.barrera.oro@gmail.com <ivan.barrera.oro@gmail.com@d83696aa-246a-b927-6463-8bf85c3dbaf6>
2014-11-15 00:00:49 +0000

    chat mejorado, incorporado en saper.php

--- website/lib/php/ext/freichat/hardcode.php
+++ website/lib/php/ext/freichat/hardcode.php
@@ -1,9 +1,31 @@
 <?php
+/*
+ * ¡¡NO MODIFICAR!!
+ */
 require_once 'load.php';
-$session = new Session;
-$session->useSystemPassword();
-
-$db = new DB(SMP_DB_CHARSET);
-
-$usuario = new Usuario($db, $session->retrieveEnc(SMP_SESSINDEX_USERNAME));
-$chat = new Chat(SMP_DB_HOST, SMP_DB_NAME, SMP_DB_USER_CHAT, SMP_DB_PASS_CHAT, SMP_CHAT_ADMINPASS, $usuario->getUsuarioId());
+$dsn = 'mysql:host=' . SMP_DB_HOST . ';dbname=' . SMP_DB_NAME; //DSN
+$db_user = SMP_DB_USER_CHAT; //DB username
+$db_pass = SMP_DB_PASS_CHAT; //DB password
+$driver = 'Custom'; //Integration driver
+$db_prefix = ''; //prefix used for tables in database
+$uid = Chat::generateUid(); //Any random unique number
+$connected='YES';
+$PATH = 'freichat/'; 
+$installed = TRUE; 
+$admin_pswd = SMP_CHAT_ADMINPASS; 
+$debug = FALSE;
+$custom_error_handling='YES'; 
+$use_cookie='false';
+$smtp_username = SMP_EMAIL_USER;
+$smtp_password = SMP_EMAIL_PSWD;
+$force_load_jquery = 'NO';
+$usertable='frei_users';
+$row_username='Nombre';
+$row_userid='Id';
+$avatar_table_name='members';
+$avatar_column_name='avatar';
+$avatar_userid='id';
+$avatar_reference_user='id';
+$avatar_reference_avatar='id';
+//to avoid unnecessary file changes , *do not change
+$avatar_field_name=$avatar_column_name; //
